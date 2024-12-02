ba62e4902fb95aedb3791ef0a4ca8d187b5b20c1
Aryan Kumar <89503177+MartinLegend24@users.noreply.github.com>
2024-12-02 19:47:33 +0530

    Update ElectriciyUsageMLProject.py

--- ElectriciyUsageMLProject.py
+++ ElectriciyUsageMLProject.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import numpy as np #optional [You can import it if you wish to use it]
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
